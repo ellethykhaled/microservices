036319fd5f69a9037732d0f0097318333f23d4d1
ellethykhaled <ellethykhaled2@gmail.com>
2023-09-30 14:24:10 +0300

    Limits Service Project

---
+++ 01-limits-service/src/main/java/com/lethy/microservices/MicroservicesConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.lethy.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesConfigurationApplication.class, args);
+	}
+
+}
---
+++ 01-limits-service/src/test/java/com/lethy/microservices/MicroservicesConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lethy.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
